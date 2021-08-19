c2a765c4b2e9d0179f1c1dbe79cf8d3b45e1ed41
Vitaliy Demeshko <DemeshkoVitaliy@gmail.com>
2021-08-19 11:22:44 +0300

    first changes

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -3,7 +3,6 @@ import styleProfile from "./Profile.module.css"
 import ProfileInfo from "./ProfileInfo/ProfileInfo"
 import MyPosts from "./MyPosts/MyPosts"
 
-
 const Profile = (props:any) => {
     return (
         <div className={styleProfile.profileStyles}>
